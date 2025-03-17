2ab9d516ff5295f0ed804ecaf1471abad423065d
NGUYENDINHHUNG <Nguyenhung21001@gmail.com>
2025-03-18 00:09:45 +0700

    commit product page

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,6 +1,13 @@
-import React from "react";
-const Product = () => {
-  return <h1 className="text-3xl font-bold ">Product</h1>;
-};
+"use client";
+import Banner from "@/components/products/banner";
+import ListProduct from "@/components/products/list-product";
 
-export default Product;
+export default function Product() {
+  return (
+    <div className="space-y-6">
+      <h1 className="text-3xl font-bold tracking-tight mb-8">Product</h1>
+      <Banner />
+      <ListProduct />
+    </div>
+  );
+}
